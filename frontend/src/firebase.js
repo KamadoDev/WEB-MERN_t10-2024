279905159fbe82fd730bbd52c35b1e187a3f4f63
KamadoDev <kamado.nql2109@gmail.com>
2025-10-09 00:14:03 +0700

    fix auth google

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -11,11 +11,14 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_SENDERID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+const firebaseApp = initializeApp(firebaseConfig); // Đổi tên biến từ 'app' thành 'firebaseApp'
+const analytics = getAnalytics(firebaseApp); // Sử dụng tên mới
+
+// SỬA: Xuất firebaseApp thay vì app.
+export { firebaseApp, analytics }; 
